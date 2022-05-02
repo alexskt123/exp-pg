599acc7667afda6652b377fd5f0a33df718fbe23
Alex So <alexskt123@gmail.com>
2022-05-02 13:31:42 +0800

    eslint

--- order/index.js
+++ order/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = new express.Router();
 
 router.get('/order', function (req, res) {
-   //code 
-   res.send('order');
+  //code
+  res.send('order');
 });
 
 module.exports = router;
--- user/index.js
+++ user/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = new express.Router();
 
 router.get('/user', function (req, res, next) {
-   //code 
-   res.send({
-      user: 'ABC',
-      name: 'GGGG'
-   });
-   console.log('aa')
-   next();
+  //code
+  res.send({
+    user: 'ABC',
+    name: 'GGGG'
+  });
+  console.log('aa')
+  next();
 });
 
 module.exports = router;
